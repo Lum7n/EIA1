2901ed7b3f0e9f6ed13dcca55390d38bd151347a
Lum7n <56406099+Lum7n@users.noreply.github.com>
2019-12-04 18:02:42 +0100

    Add files via upload

---
+++ Aufgabe7/script.ts
@@ -0,0 +1,40 @@
+window.addEventListener("load", function () {
+    document.querySelector("#btn1").addEventListener("mousedown", function () { playSample("kick.mp3") });
+    document.querySelector("#btn2").addEventListener("mousedown", function () { playSample("snare.mp3") });
+    document.querySelector("#btn3").addEventListener("mousedown", function () { playSample("hihat.mp3") });
+
+    document.querySelector("#btn4").addEventListener("mousedown", function () { playSample("F.mp3") });
+    document.querySelector("#btn5").addEventListener("mousedown", function () { playSample("G.mp3") });
+    document.querySelector("#btn6").addEventListener("mousedown", function () { playSample("A.mp3") });
+
+    document.querySelector("#btn7").addEventListener("mousedown", function () { playSample("C.mp3") });
+    document.querySelector("#btn8").addEventListener("mousedown", function () { playSample("laugh-1.mp3") });
+    document.querySelector("#btn9").addEventListener("mousedown", function () { playSample("laugh-2.mp3") });
+
+    document.querySelector("#play").addEventListener("click", start_beat)
+})
+
+function playSample(file): void {
+    var sound: HTMLAudioElement = new Audio("sounds/" + file);
+    sound.play();
+}
+
+//function playSample(string): void {
+//   var sound: HTMLAudioElement = new Audio(a"sounds/kick.mp3", b "sounds/A.mp3");
+//    sound.play();}
+
+
+function start_beat() {
+    var beat: string[] = ["sounds/kick.mp3", "sounds/snare.mp3", "sounds/kick.mp3", "sounds/kick.mp3", "sounds/hihat.mp3"]
+    var index: number = 0;
+    var time = setInterval (play_beat, 400);
+    function play_beat() {
+        var sound: HTMLAudioElement = new Audio(beat[index]);
+        sound.play();
+        index++;
+        if (index > 4) index = 0;
+    }
+}
+
+
+
